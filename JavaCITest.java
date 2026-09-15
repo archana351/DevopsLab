0b2f2a7491c37c0fb9e913ab533f1a90f92644e8
KesaraAnanya <ananyakesara@gmail.com>
2026-09-15 14:56:58 +0530

    added line2

--- JavaCITest.java
+++ JavaCITest.java
@@ -2,5 +2,6 @@ public class JavaCITest{
     public static void main(String[] args) {
         System.out.println("Test CI Integration");
         System.out.println("added new line");
+        System.out.println("added line2");
     }
 }
